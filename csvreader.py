384b3b080efe11265beba55404a91507772256df
AaronCampbell02 <aaronc2006@outllook.com>
2024-08-05 14:38:06 +0100

    Use of date instead of string to make use of formatting and other inbuilt methods

--- csvreader.py
+++ csvreader.py
@@ -1,5 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import date
 
 x= []
 y =[]
@@ -15,45 +17,25 @@ with open('games.csv','r') as csvfile:
 
     for row in data:
         month = int(row[1])
-        print(month)
-        year = row[0]
-        if month == 1:
-            month_name = "January"
-        elif month == 2:
-            month_name = "February"
-        elif month == 3:
-            month_name = "March"
-        elif month == 4:
-            month_name = "April"
-        elif month == 5:
-            month_name = "May"
-        elif month == 6:
-            month_name = "June"
-        elif month == 7:
-            month_name = "July"
-        elif month == 8:
-            month_name = "August"
-        elif month == 9:
-            month_name = "September"
-        elif month == 10:
-            month_name = "October"
-        elif month == 11:
-            month_name = "November"
-        elif month == 12:
-            month_name = "December"
+        year = int(row[0])
 
-        date = month_name + str(year)
-        x.append(date)
+        date_time_value = mdates.date2num(date(year,month,1))
+        formatter = mdates.ConciseDateFormatter(date_time_value)
+
+        x.append(date_time_value)
         y.append(int(row[2]))
+         
+
          
 plt.figure(figsize=(12, 8))   
 plt.plot(x, y, color = 'g',linestyle = 'dashed', 
-         marker = 'o', label = "Games") 
+         marker = 'o', label = "Games")
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval = 3)) 
 plt.xlabel('Month') 
 plt.ylabel('Games') 
 plt.title('Number of games per month') 
 plt.legend()
-plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 
 plt.savefig('games_per_month.png', format='png')
